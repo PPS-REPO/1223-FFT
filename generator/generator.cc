435b77596eba565c620c7a1ee639486ad570f4ee
pps-admin <pps.github@gmail.com>
2025-06-02 18:30:53 +0900

    Update: path 'generator/generator.cc' - Updated by 'nNaxhO'

--- generator/generator.cc
+++ generator/generator.cc
@@ -1,20 +1,18 @@
 #include "testlib.h"
+#include <iostream>
+#include <iomanip>
+#include <random>
 
 using namespace std;
 
 int main(int argc, char** argv) {
-    int al, ar, bl, br, a, b;
-    
-    registerGen(argc, argv, 1);
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_real_distribution<float> dist(-1.0f, 1.0f);
+    std::cout << std::fixed << std::setprecision(4);
+    for (int i = 0; i < 256; ++i)
+        cout << dist(gen) << " ";
+    cout << endl;
 
-    al = atoi(argv[1]);
-    ar = atoi(argv[2]);
-    bl = atoi(argv[3]);
-    br = atoi(argv[4]);
-
-    a = rnd.next(al, ar);
-    b = rnd.next(bl, br);
-
-    printf("%d %d\n", a, b);
     return 0;
 }
