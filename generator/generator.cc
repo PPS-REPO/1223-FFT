39a24a6f34f731f9d9abbd255830c87d6a4f8fd0
pps-admin <pps.github@gmail.com>
2025-11-30 22:41:50 +0900

    Update: path 'generator/generator.cc' - Updated by 'nNaxhO'

--- generator/generator.cc
+++ generator/generator.cc
@@ -3,20 +3,20 @@
 #include <random>
 
 int main() {
-    ios_base::sync_with_stdio(false);
+    std::ios_base::sync_with_stdio(false);
     std::cout << std::fixed << std::setprecision(4);
 
     std::random_device rd;
     std::mt19937 gen(rd());
-    std::uniform_real_distribution<int> Samplerate(1, 786000);
-    std::uniform_real_distribution<int> BufferSize(0, 10);
+    std::uniform_int_distribution<int> Samplerate(1, 786000);
+    std::uniform_int_distribution<int> BufferSize(0, 10);
     std::uniform_real_distribution<double> Sample(-1.0, 1.0);
 
     int SR = Samplerate(gen);
     std::cout << SR << std::endl;
     std::cout << (1<<BufferSize(gen)) << std::endl;
     for(int i = 0; i < SR; ++i)
-        std::cout << Samplerate(gen) << (i+1 == SR ? '' : ' ');
+        std::cout << Sample(gen) << (i+1 == SR ? "" : " ");
     std::cout << std::flush;
 
     return 0;
