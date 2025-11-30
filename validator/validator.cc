e51e68718b5185684c6574aa8e451f4c0086288a
pps-admin <pps.github@gmail.com>
2025-11-30 23:30:51 +0900

    Update: path 'validator/validator.cc' - Updated by 'nNaxhO'

--- validator/validator.cc
+++ validator/validator.cc
@@ -1,16 +1,34 @@
 #include "testlib.h"
-using namespace std;
+#include <cmath>
 
-#define A_MIN 1
-#define A_MAX 9
-#define B_MIN 1
-#define B_MAX 9
-
-int main(int argc, char** argv) {
+int main(int argc, char* argv[]) {
     registerValidation(argc, argv);
-    inf.readInt(A_MIN, A_MAX, "A");
-    inf.readSpace();
-    inf.readInt(B_MIN, B_MAX, "B");
+    
+    int samplerate = inf. readInt(1, 768000, "samplerate");
     inf.readEoln();
+    
+    int buffersize = inf.readInt(1, 1024, "buffersize");
+    inf.readEoln();
+    
+    bool isPowerOfTwo = (buffersize > 0) && ((buffersize & (buffersize - 1)) == 0);
+    ensuref(isPowerOfTwo, "buffersize must be a power of 2");
+    
+    int numSamples = 1000 * samplerate;
+    
+    for (int i = 0; i < numSamples; i++) {
+        double sample = inf.readDouble(-1. 0, 1.0, "sample");
+        
+        double rounded = round(sample * 10000. 0) / 10000.0;
+        ensuref(fabs(sample - rounded) < 1e-9, 
+                "sample must have at most 4 decimal places");
+        
+        if (i < numSamples - 1) {
+            inf. readSpace();
+        }
+    }
+    inf.readEoln();
+    
     inf.readEof();
+    
+    return 0;
 }
